bf157110a28f91174f956a8082dbb5ff3e746ca6
donadams50 <sumbomatic@gmail.com>
2020-07-28 09:37:11 +0100

    update

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ var app = require("../index.js");
 describe("GET /", function() {
  it("respond with hello world", function(done) {
  //navigate to root and check the the response is "hel world"
- request(app).get("/").expect("hello subo", done);
+ request(app).get("/").expect("hello sub", done);
  });
 });
